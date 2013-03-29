a695e4f95bc3ef3b2698961ff2f91b186f88464d
Stephan Brenner <admin@ubuntu.(none)>
2013-03-29 16:37:32 -0700

    Interface design

--- test/main.js
+++ test/main.js
@@ -1,10 +1,12 @@
 var should = require('should');
 var proxy = require('../lib/main');
+var io = require('socket.io-client');
 
 describe('proxy.connect', function() {
     describe('with no arguments', function() {
         it('connects to the server specified in the HTTP_PROXY environment variable', function() {
-            proxy.connect();
+            var socket = proxy.connect('http://destination');
+            should.exist(socket);
         });
     });
 });
@@ -12,15 +14,9 @@ describe('proxy.connect', function() {
 describe('proxy.connect', function() {
     describe('with proxy server URL', function() {
         it('connects to the specified proxy server', function() {
-            proxy.connect('http://proxy:8080');
-        });
-    });
-});
-
-describe('proxy.createSocket', function() {
-    describe('with no arguments', function() {
-        it('returns a socket', function() {
-            proxy.createSocket();
+            proxy.init('http://proxy.8080');
+            var socket = proxy.connect('http://destination');
+            should.exist(socket);
         });
     });
 });
