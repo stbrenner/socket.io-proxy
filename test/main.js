0aadf65b97666d997b6ab3517cd255ccddc5f836
Stephan Brenner <ymx@server.fake>
2013-04-02 15:35:09 -0700

    Extended unit tests

--- test/main.js
+++ test/main.js
@@ -4,15 +4,23 @@ var io = require('socket.io-client');
 
 describe('proxy.connect', function() {
     describe('with no arguments', function() {
-        it('connects to the server specified in the http_proxy environment variable', function() {
-            process.env.http_proxy = 'http://proxy:8081';
-            var socket = proxy.connect('http://destination');
-            should.exist(socket);
+        describe('and http_proxy env variable defined', function() {
+            it('connects to the destination through the proxy', function() {
+                process.env.http_proxy = 'http://proxy:8081';
+                proxy.init();
+                var socket = proxy.connect('https://destination');
+                should.exist(socket);
+            });
+        });
+        describe('and http_proxy env variable not defined', function() {
+            it('directly connects to the destination', function() {
+                process.env.http_proxy = '';
+                proxy.init();
+                var socket = proxy.connect('http://destination');
+                should.exist(socket);
+            });
         });
     });
-});
-
-describe('proxy.connect', function() {
     describe('with proxy server URL', function() {
         it('connects to the specified proxy server', function() {
             proxy.init('http://proxy:8080');
